5e087897ce3dfcc9f04eecb31e094457fb1913dd
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-01-22 23:12:33 +0300

    remove: uninformative comments for front-end

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,7 +15,7 @@ export default function Home({ username }) {
 
   useEffect(() => {
     scrl.current.scrollIntoView({ behavior: "smooth" });
-  }, [messages]) // messages arrayine giriş çıkış olduğunda display edilen veriyi takip et. scrollIntoView.
+  }, [messages]) 
 
   
   const { sendMessage } = useWebSocket(WS_URL, {
@@ -36,7 +36,7 @@ export default function Home({ username }) {
 
   useEffect(() => {
     window.onbeforeunload = () => {return '';}
-  }, []) // Herhangi bir şey tetiklemez.
+  }, []) 
   
   useEffect(() => {
     if (ReadyState.OPEN) {
@@ -45,19 +45,19 @@ export default function Home({ username }) {
   }, [])
 
   
-  const THROTTLE = 500; // server'a en hızlı 500 ms aralıklarla mesaj gönderebiliriz.
-  const sendMessageThrottled = useRef(throttle((msg) => { // useRef optimizasyon sağlar ve throttle'ın tekrar create edilerek ms sınırının bozulmasını engeller.
+  const THROTTLE = 500; 
+  const sendMessageThrottled = useRef(throttle((msg) => { 
     sendMessage(JSON.stringify({type: 'chat-message', msg: msg}));
   }, THROTTLE));
 
   const handleSendMessage = () => {
-    if (message.trim()) { // message'ın başındaki ve sonundaki boş karakterleri sil.
+    if (message.trim()) { 
       sendMessageThrottled.current(message);
-      setMessage(""); // mesaj kutusunu sıfırlamak için.
+      setMessage(""); 
     }
   };
 
-  const handleKeyPress = (e) => { // Enter ile gönderim sağlama.
+  const handleKeyPress = (e) => { 
     if (e.key === 'Enter') {
       e.preventDefault();
       handleSendMessage();
@@ -84,18 +84,15 @@ export default function Home({ username }) {
           ))}
         </div>
         <div className="bg-gray-950 w-3/4 float-left overflow-y-auto scroll-smooth rounded-xl max-w-full text-center mx-auto my-3 container message">
-          {/* Eğer duplicated true dönerse yani bir kullanıcı adı iki clientte de kullanılmaya çalışılıyorsa ikinci clientteki görüntüleneck olan: */}
           {duplicated ? (
             <div className='bg-red-600 first:mt-4 my-2 max-w-full text-left  rounded mx-4 mt-2'>
               <p className='text-sm font-bold pl-1 bg-red-800 max-w-fit mx-1 px-1 rounded py-0 text-white'><code>SERVER</code></p>
-              <p className='text-md pl-3 pb-2'><b>Another user is active now with this username :) You can reload and choose a unique username</b></p>
+              <p className='text-md pl-3 pb-2'><b>Another user is active now with this username. You can reload and choose a unique username</b></p>
             </div>
           ) : (
             <>
-              {/* 'iki noktadan sonrası' duplicated false dönerse, messages arrayinin içerisindeki verileri gez, index ile sıralanacak div'in içerisine mesaj verilerini yerleştir. */}
               {messages.map((msg, index) => (
                 <div key={index} className='bg-gray-800 overflow-hidden first:mt-4 my-2 min-h-max:  max-w-full text-left  rounded mx-4 mt-2'>
-                  {/* msg parametresinden dönen JSON tipi veriyi parse et ve disconnectedUser field'ını oku, eğer boşsa normal şekilde mesaj verilerini yazdır. */}
                   {JSON.parse(msg).disconnectedUser === '' ? (
                     <>
                       <p className='text-sm font-bold pl-1 bg-cyan-600 max-w-fit mx-1 px-1 rounded py-0 text-white'><code>{JSON.parse(msg).author}</code></p>
@@ -104,7 +101,6 @@ export default function Home({ username }) {
                     </>
                   ) : (
                     <>
-                      {/* Boş değilse disconnectedUser'ın username'ini (servardan oto gönderim) ve mesaj author olarak (SERVER) yazısı ile birlikte kanaldan bağlantısı kesildi mesajı display et */}
                       <p className='text-sm font-bold pl-1 bg-red-400 max-w-fit mx-1 px-1 rounded py-0 text-white'><code>{JSON.parse(msg).author}</code></p>
                       <p className='text-md pl-3 pb-2'>{JSON.parse(msg).disconnectedUser} disconnected from channel</p>
                       <p className='text-sm font-bold pl-1 float-right bg-blue-400 max-w-fit mx-1 px-1 rounded py-0 text-black'><code>{JSON.parse(msg).time}</code></p>
@@ -112,20 +108,17 @@ export default function Home({ username }) {
                   )}
                 </div>
               ))}
-              {/* referans kullanılarak takip edilecek div: */}
               <div ref={scrl} />
             </>
           )}
         </div>
       </div>
 
-      {/* Eğer duplicated true dönerse, form alanını oluşturma. */}
       {duplicated ? (
         <div>
         </div>
       ) : (
         <>
-          {/* Eğer duplicated false dönerse, formu oluştur. */}
           <form className='align-bottom flex'>
             <input
               type="text"
